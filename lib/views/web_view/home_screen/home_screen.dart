f069370eb6f5116aef8e47f4dd78fa8887f71c4c
Cemal Faruk Güney <63407686+cemalfarukguney@users.noreply.github.com>
2021-12-17 00:07:02 +0300

    Left App Bar onTap mehods improved
    When menu items on the left app bar are clicked the menu item is displayed as selected and the middle of the screen changes.

--- lib/views/web_view/home_screen/home_screen.dart
+++ lib/views/web_view/home_screen/home_screen.dart
@@ -2,73 +2,183 @@ import 'package:flutter/material.dart';
 import 'widgets/app_bar.dart';
 import 'widgets/top_bar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final PageController _pageController = PageController(initialPage: 0);
+  int _currentIndex = 0;
+  @override
+  void dispose(){
+    super.dispose();
+    _pageController.dispose();
+  }
+  void changePage(){
+    _pageController.jumpToPage(_currentIndex);
+  }
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-        body: Container(
-      width: size.width,
-      height: size.height,
-      color: Color.fromRGBO(244, 226, 198, 1),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          TopBar(size: size),
-          Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
-            const SizedBox(
-              width: 200,
-              child: LeftAppBar(),
-            ),
-            Container(
-                //POST
+      body: Container(
+        width: size.width,
+        height: size.height,
+        color: Color.fromRGBO(244, 226, 198, 1),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            TopBar(size: size),
+            Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+              SizedBox(
+                width: 200,
+                child: LeftAppBar(currentIndex: _currentIndex, onTap: (index) {setState(() {
+                  _currentIndex = index;
+                  _pageController.jumpToPage(_currentIndex);
+                });}),
+              ),
+              SizedBox(
                 width: (size.width - 400),
                 height: size.height - 150,
-                alignment: Alignment.bottomCenter,
+                child: PageView(
+                  controller: _pageController,
+                  children: <Widget>[
+                    Container(
+                      //POST Page View +Page view Controller 
+                      width: (size.width - 400),
+                      height: size.height - 150,
+                      alignment: Alignment.bottomCenter,
+                      color: Color.fromRGBO(244, 226, 198, 1),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            for (int i = 0; i < 100; i++)Center(
+                              child: Text(
+                              'POST' + i.toString(),
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 30),
+                              )
+                            ),
+                          ],
+                        ),
+                      )
+                    ),
+                    Container(
+                      //POST Page View +Page view Controller 
+                      width: (size.width - 400),
+                      height: size.height - 150,
+                      alignment: Alignment.bottomCenter,
+                      color: Color.fromRGBO(244, 226, 198, 1),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            for (int i = 0; i < 80; i++)Center(
+                              child: Text(
+                              'POST' + i.toString(),
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 30),
+                              )
+                            ),
+                          ],
+                        ),
+                      )
+                    ),
+                    Container(
+                      //POST Page View +Page view Controller 
+                      width: (size.width - 400),
+                      height: size.height - 150,
+                      alignment: Alignment.bottomCenter,
+                      color: Color.fromRGBO(244, 226, 198, 1),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            for (int i = 0; i < 60; i++)Center(
+                              child: Text(
+                              'POST' + i.toString(),
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 30),
+                              )
+                            ),
+                          ],
+                        ),
+                      )
+                    ),
+                    Container(
+                      //POST Page View +Page view Controller 
+                      width: (size.width - 400),
+                      height: size.height - 150,
+                      alignment: Alignment.bottomCenter,
+                      color: Color.fromRGBO(244, 226, 198, 1),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            for (int i = 0; i < 40; i++)Center(
+                              child: Text(
+                              'POST' + i.toString(),
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 30),
+                              )
+                            ),
+                          ],
+                        ),
+                      )
+                    ),
+                    Container(
+                      //POST Page View +Page view Controller 
+                      width: (size.width - 400),
+                      height: size.height - 150,
+                      alignment: Alignment.bottomCenter,
+                      color: Color.fromRGBO(244, 226, 198, 1),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            for (int i = 0; i < 20; i++)Center(
+                              child: Text(
+                              'POST' + i.toString(),
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 30),
+                              )
+                            ),
+                          ],
+                        ),
+                      )
+                    ),
+                  ]
+                ),
+              ),
+              Container(
+                width: 200,
+                height: size.height - 150,
+                alignment: Alignment.topCenter,
                 color: Color.fromRGBO(244, 226, 198, 1),
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      for (int i = 0; i < 100; i++)Center(
-                          child: Text(
-                        'POST' + i.toString(),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 30),
-                      )),
-                    ],
-                  ),
-                )),
-            Container(
-              width: 200,
-              height: size.height - 150,
-              alignment: Alignment.topCenter,
-              color: Color.fromRGBO(244, 226, 198, 1),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      Text(
-                        'Suggested Clubs',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 24, color: Color.fromRGBO(94, 119, 3, 1)),
-                      ),
-                      
-                      for (int i = 0; i < 20; i++) Text(
-                        'Club' + i.toString(),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 18, color: Colors.grey[600]),
-                      ),
-                    ],
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Text(
+                          'Suggested Clubs',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 24, color: Color.fromRGBO(94, 119, 3, 1)),
+                        ),
+                        
+                        for (int i = 0; i < 20; i++) Text(
+                          'Club' + i.toString(),
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ]),
-        ],
-      ),
-    ));
+            ]),
+          ],
+        ),
+      )
+    );
   }
 }
